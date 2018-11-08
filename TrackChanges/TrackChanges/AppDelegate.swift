8b365385491cd6950ff1164c676421fb63f60490
Pavly Habashy <phabashy@usc.edu>
2018-11-08 15:25:32 -0800

    Connect working

--- TrackChanges/TrackChanges/AppDelegate.swift
+++ TrackChanges/TrackChanges/AppDelegate.swift
@@ -1,100 +1,90 @@
-//
-//  AppDelegate.swift
-//  TrackChanges
-//
-//  Created by Nolan Earl on 10/13/18.
-//  Copyright © 2018 TrackChanges. All rights reserved.
-//
-
 import UIKit
-import CoreData
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate  {
+class AppDelegate: UIResponder,
+UIApplicationDelegate, SPTAppRemoteDelegate {
+    
+    fileprivate let redirectUri = URL(string: "trackchanges://spotify-login-callback/")!
+    fileprivate let clientIdentifier = "4bebf0c82b774aaa99764eb7c5c58cc4"
+    fileprivate let name = "Now Playing View"
+    
+    // keys
+    static fileprivate let kAccessTokenKey = "access-token-key"
+    
+    var accessToken = UserDefaults.standard.string(forKey: kAccessTokenKey) {
+        didSet {
+            let defaults = UserDefaults.standard
+            defaults.set(accessToken, forKey: AppDelegate.kAccessTokenKey)
+            defaults.synchronize()
+        }
+    }
+    
+    
+    var playerViewController: ConnectViewController {
+        get {
+            return self.window?.rootViewController as! ConnectViewController
+        }
+    }
     
     var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    lazy var appRemote: SPTAppRemote = {
+        let configuration = SPTConfiguration(clientID: self.clientIdentifier, redirectURL: self.redirectUri)
+        let appRemote = SPTAppRemote(configuration: configuration, logLevel: .debug)
+        appRemote.connectionParameters.accessToken = self.accessToken
+        appRemote.delegate = self
+        return appRemote
+    }()
+    
+    class var sharedInstance: AppDelegate {
+        get {
+            return UIApplication.shared.delegate as! AppDelegate
+        }
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
+        let parameters = appRemote.authorizationParameters(from: url);
+        
+        if let access_token = parameters?[SPTAppRemoteAccessTokenKey] {
+            appRemote.connectionParameters.accessToken = access_token
+            self.accessToken = access_token
+        } else if let error_description = parameters?[SPTAppRemoteErrorDescriptionKey] {
+//            playerViewController.showError(error_description);
+        }
+        
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        
-        if ConnectViewController().appRemote.isConnected {
-            ConnectViewController().appRemote.disconnect()
-        }
+//        playerViewController.appRemoteDisconnect()
+        appRemote.disconnect()
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        
-        if let _ = ConnectViewController().appRemote.connectionParameters.accessToken {
-            ConnectViewController().appRemote.connect()
-        }
+        self.connect();
     }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
+    
+    func connect() {
+//        playerViewController.appRemoteConnecting()
+        appRemote.connect()
     }
-
-    // MARK: - Core Data stack
-
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
-        let container = NSPersistentContainer(name: "TrackChanges")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-
-    // MARK: - Core Data Saving support
-
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
+    
+    // MARK: AppRemoteDelegate
+    
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+        self.appRemote = appRemote
+//        playerViewController.appRemoteConnected()
+        playerViewController.performSegue(withIdentifier: "GoToFeed", sender: ConnectViewController())
     }
-
+    
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+        print("didFailConnectionAttemptWithError")
+//        playerViewController.appRemoteDisconnect()
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
+        print("didDisconnectWithError")
+//        playerViewController.appRemoteDisconnect()
+    }
+    
 }
-
--- TrackChanges/TrackChanges/ViewControllers/ConnectViewController.swift
+++ TrackChanges/TrackChanges/ViewControllers/ConnectViewController.swift
@@ -10,34 +10,65 @@ import UIKit
 import StoreKit
 import MediaPlayer
 
-class ConnectViewController: UIViewController, SPTSessionManagerDelegate, SPTAppRemoteDelegate, SPTAppRemotePlayerStateDelegate {
+class ConnectViewController: UIViewController, SPTAppRemotePlayerStateDelegate,
+    SPTAppRemoteUserAPIDelegate,
+SKStoreProductViewControllerDelegate {
+    
+    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
+        self.playerState = playerState
+        updateViewWithPlayerState(playerState)
+    }
+    
+    // MARK: - <SPTAppRemoteUserAPIDelegate>
+    
+    func userAPI(_ userAPI: SPTAppRemoteUserAPI, didReceive capabilities: SPTAppRemoteUserCapabilities) {
+//        updateViewWithCapabilities(capabilities)
+    }
+    
 
     @IBOutlet weak var connectButton: UIButton!
     
-    let SpotifyClientID = "4bebf0c82b774aaa99764eb7c5c58cc4"
-    let SpotifyRedirectURL = URL(string: "trackchanges://spotify-login-callback/")!
+//    let SpotifyClientID = "4bebf0c82b774aaa99764eb7c5c58cc4"
+//    let SpotifyRedirectURL = URL(string: "trackchanges://spotify-login-callback/")!
     
-    lazy var configuration = SPTConfiguration(
-        clientID: SpotifyClientID,
-        redirectURL: SpotifyRedirectURL
-    )
+    fileprivate let playURI = "spotify:album:5uMfshtC2Jwqui0NUyUYIL"
+    fileprivate let trackIdentifier = "spotify:track:32ftxJzxMPgUFCM6Km9WTS"
+    fileprivate let name = "Now Playing View"
     
-    lazy var sessionManager: SPTSessionManager = {
-        if let tokenSwapURL = URL(string: "https://trackchanges.herokuapp.com/api/token"),
-            let tokenRefreshURL = URL(string: "https://trackchanges.herokuapp.com/api/refresh_token") {
-            self.configuration.tokenSwapURL = tokenSwapURL
-            self.configuration.tokenRefreshURL = tokenRefreshURL
-            self.configuration.playURI = ""
-        }
-        let manager = SPTSessionManager(configuration: self.configuration, delegate: self)
-        return manager
-    }()
     
-    lazy var appRemote: SPTAppRemote = {
-        let appRemote = SPTAppRemote(configuration: self.configuration, logLevel: .debug)
-        appRemote.delegate = self
-        return appRemote
-    }()
+    fileprivate func updateViewWithPlayerState(_ playerState: SPTAppRemotePlayerState) {
+//        updatePlayPauseButtonState(playerState.isPaused)
+//        updateRepeatModeLabel(playerState.playbackOptions.repeatMode)
+//        updateShuffleLabel(playerState.playbackOptions.isShuffling)
+//        trackNameLabel.text = playerState.track.name + " - " + playerState.track.artist.name
+//        fetchAlbumArtForTrack(playerState.track) { (image) -> Void in
+//            self.updateAlbumArtWithImage(image)
+//        }
+//        updateViewWithRestrictions(playerState.playbackRestrictions)
+    }
+
+    
+//    lazy var configuration = SPTConfiguration(
+//        clientID: SpotifyClientID,
+//        redirectURL: SpotifyRedirectURL
+//    )
+//
+//    lazy var sessionManager: SPTSessionManager = {
+//        if let tokenSwapURL = URL(string: "https://trackchanges.herokuapp.com/api/token"),
+//            let tokenRefreshURL = URL(string: "https://trackchanges.herokuapp.com/api/refresh_token") {
+//            self.configuration.tokenSwapURL = tokenSwapURL
+//            self.configuration.tokenRefreshURL = tokenRefreshURL
+//            self.configuration.playURI = ""
+//        }
+//        let manager = SPTSessionManager(configuration: self.configuration, delegate: self)
+//        return manager
+//    }()
+//
+//    lazy var appRemote: SPTAppRemote = {
+//        let appRemote = SPTAppRemote(configuration: self.configuration, logLevel: .debug)
+//        appRemote.delegate = self
+//        return appRemote
+//    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,57 +84,96 @@ class ConnectViewController: UIViewController, SPTSessionManagerDelegate, SPTApp
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        if appRemote.isConnected {
-            self.performSegue(withIdentifier: "GoToFeed", sender: nil)
-        } else {
-            print("-----NOT CONNECTED------")
-        }
+       
     }
     
     @IBAction func connectToSpotify(_ sender: Any) {
-        let requestedScopes: SPTScope = [.appRemoteControl]
-        self.sessionManager.initiateSession(with: requestedScopes, options: .default)
-    }
-    
-    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        self.sessionManager.application(app, open: url, options: options)
-        return true
-    }
-    
-    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
-        print("success", session)
-        self.appRemote.connectionParameters.accessToken = session.accessToken
-        self.appRemote.connect()
-    }
-    
-    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
-        print("fail", error)
-    }
-    
-    func sessionManager(manager: SPTSessionManager, didRenew session: SPTSession) {
-        print("renewed", session)
-    }
-    
-    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
-        print("connected")
-        self.appRemote.playerAPI?.delegate = self
-        self.appRemote.playerAPI?.subscribe(toPlayerState: { (result, error) in
-            if let error = error {
-                debugPrint(error.localizedDescription)
+//        let requestedScopes: SPTScope = [.appRemoteControl]
+//        self.sessionManager.initiateSession(with: requestedScopes, options: .default)
+        
+        if !(appRemote.isConnected) {
+            if (!appRemote.authorizeAndPlayURI(playURI)) {
+                // The Spotify app is not installed, present the user with an App Store page
+//                showAppStoreInstall()
             }
-        })
+        } else if playerState == nil || playerState!.isPaused {
+            startPlayback()
+        } else {
+            pausePlayback()
+        }
     }
-    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
-        print("disconnected")
-    }
-    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
-        print("failed")
-    }
-    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
-        print("player state changed")
+
+    fileprivate var playerState: SPTAppRemotePlayerState?
+    
+    var appRemote: SPTAppRemote {
+        get {
+            return AppDelegate.sharedInstance.appRemote
+        }
     }
     
-    /*
+    fileprivate func startPlayback() {
+        appRemote.playerAPI?.resume(defaultCallback)
+    }
+    
+    fileprivate func pausePlayback() {
+        appRemote.playerAPI?.pause(defaultCallback)
+    }
+
+    var defaultCallback: SPTAppRemoteCallback {
+        get {
+            return {[weak self] _, error in
+                if let error = error {
+//                    self?.displayError(error as NSError)
+                }
+            }
+        }
+}
+
+//    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+//        self.sessionManager.application(app, open: url, options: options)
+//        return true
+//    }
+//
+//    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+//        print("success", session)
+//        self.appRemote.connectionParameters.accessToken = session.accessToken
+//        self.appRemote.connect()
+//    }
+//
+//    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
+//        print("fail", error)
+//    }
+//
+//    func sessionManager(manager: SPTSessionManager, didRenew session: SPTSession) {
+//        print("renewed", session)
+//    }
+//
+//    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+//        print("connected")
+//        self.appRemote.playerAPI?.delegate = self
+//        self.appRemote.playerAPI?.subscribe(toPlayerState: { (result, error) in
+//            if let error = error {
+//                debugPrint(error.localizedDescription)
+//            }
+//        })
+//
+//        if appRemote.isConnected {
+//            self.performSegue(withIdentifier: "GoToFeed", sender: nil)
+//        } else {
+//            print("-----NOT CONNECTED------")
+//        }
+//    }
+//    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
+//        print("disconnected")
+//    }
+//    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+//        print("failed")
+//    }
+//    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
+//        print("player state changed")
+//    }
+
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -111,6 +181,6 @@ class ConnectViewController: UIViewController, SPTSessionManagerDelegate, SPTApp
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+ 
 
 }
