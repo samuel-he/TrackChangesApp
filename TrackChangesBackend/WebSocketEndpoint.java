05ceaecff908a3cbabe0a7f766d5163951956042
PJ Hernandez <pjhernan@usc.edu>
2018-11-12 22:00:36 -0800

    websocket needs to be tested

--- TrackChangesBackend/WebSocketEndpoint.java
+++ TrackChangesBackend/WebSocketEndpoint.java
@@ -18,13 +18,13 @@ import javax.websocket.server.ServerEndpoint;
 import org.json.simple.JSONObject;
 
 
-@ServerEndpoint (value="/ws")
+@ServerEndpoint ("/ws")
 
 public class WebSocketEndpoint {
 		
 	//Store users and their sessions in the map
 	private static final Map<String, Session> sessions = new HashMap<String, Session>();
-	private static Application application = new Application();
+	//private static Application application = new Application();
 	private static Lock lock = new ReentrantLock();
 	
 	
@@ -45,70 +45,77 @@ public class WebSocketEndpoint {
 		if (sessions.get(session.getId()) != null)
 		{
 			//TO-DO: write Application.java 
-			application.removeSession(session));
+			//application.removeSession(session));
 			sessions.remove(session.getId());
 		}
 		lock.unlock();
 	}
 	
-	//Called when a message is received by the client
-	@OnMessage
-	public void onMessage (byte[] b, Session session) {
-		lock.lock();
-		String printMe ="";
-		
-		try {
-			printMe = new String(b, "US-ASCII");
-		}catch(UnsupportedEncodingException uee) {
-			uee.printStackTrace();
-		}
-		JSONObject json = null; //need a way to parse incoming JSON
-		application.parseMessage(json, session, this);
-		
-		lock.unlock();
-		// System.out.println("onMessage::From= " + session.getId() + "Message= " + message);
+//	//Called when a message is received by the client
+//	@OnMessage
+//	public void onMessage (byte[] b, Session session) {
+//		lock.lock();
+//		String printMe ="";
+//		
+//		try {
+//			printMe = new String(b, "US-ASCII");
+//		}catch(UnsupportedEncodingException uee) {
+//			uee.printStackTrace();
+//		}
+//		//JSONObject json = null; //need a way to parse incoming JSON
+//		//application.parseMessage(json, session, this);
+//		
+//		lock.unlock();
+//		// System.out.println("onMessage::From= " + session.getId() + "Message= " + message);
+//	
+//	}
 	
-	}
+	  @OnMessage
+	    public void onMessage(String message, Session session) {
+	        System.out.println("onMessage::From=" + session.getId() + " Message=" + message);
+	        
+	        try {
+	            session.getBasicRemote().sendText("Hello Client " + session.getId() + "!");
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
    @OnError
     public void onError(Throwable e) {
        	e.printStackTrace();
     }
    
-   public void sendToSession(Session session, byte[] data) {
-	   
-	   lock.lock();
-	   try {
-		   ByteBuffer tobeSent = ByteBuffer.wrap(data);
-		   session.getBasicRemote().sendBinary(tobeSent);
-		   
-	   }catch(IOException ioe) {
-		   sessions.remove(session.getId());   
-	   }
-	   lock.unlock();
-	   
-   }
+//   public void sendToSession(Session session, byte[] data) {
+//	   
+//	   lock.lock();
+//	   try {
+//		   ByteBuffer tobeSent = ByteBuffer.wrap(data);
+//		   session.getBasicRemote().sendBinary(tobeSent);
+//		   
+//	   }catch(IOException ioe) {
+//		   sessions.remove(session.getId());   
+//	   }
+//	   lock.unlock();
+//	   
+//   }
+//   
+//   public void sendToAllSessions(Session session, byte[] data) {
+//	 
+//	   lock.lock();
+//	   try {
+//		   ByteBuffer tobeSent = ByteBuffer.wrap(data);
+//		   //loop through sessions
+//		   for (String key : sessions.keySet()) {
+//			    if (sessions.get(key)!=session) {
+//			    	sessions.get(key).getBasicRemote().sendBinary(tobeSent);
+//			    }
+//			}		   
+//	   }catch(IOException ioe) {   
+//		   ioe.printStackTrace();
+//	   }
+//	   lock.unlock();
+//	   
+//   }
    
-   public void sendToAllSessions(Session session, byte[] data) {
-	   
-	   lock.lock();
-	   try {
-		   ByteBuffer tobeSent = ByteBuffer.wrap(data);
-		   //loop through sessions
-		   for (String key : sessions.keySet()) {
-			    if (sessions.get(key)!=session) {
-			    	sessions.get(key).getBasicRemote().sendBinary(tobeSent);
-			    }
-			}		   
-	   }catch(IOException ioe) {
-		   
-		   ioe.printStackTrace();
-	   }
-	   lock.unlock();
-	   
-   }
-   
-   
-   
-
-		
+  	
 }
